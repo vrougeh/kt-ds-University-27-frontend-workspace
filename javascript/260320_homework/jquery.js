4faf615e0faa4e8729ab46b97e98bdbebbb012be
vrougeh <dlaghdrbs907@gmail.com>
2026-03-22 14:20:27 +0900

    add : add homework code

---
+++ javascript/260320_homework/jquery.js
@@ -0,0 +1,22 @@
+$().ready(function () {
+  var number = 0;
+  $(".create-content").on("click", function () {
+    if (number >= 9) {
+      alert("더 이상 추가할 수 없습니다.");
+    } else {
+      $("ul").append($("<li>").text("아이템 목록 " + ++number));
+      // console.log("아이템 추가 ", number);
+      $(".bottom-content > span").text(number);
+    }
+  });
+
+  $(".delete-all").on("click", function () {
+    if (number === 0) {
+      alert("이미 모든 아이템이 제거되었습니다.");
+    } else {
+      $("li").remove();
+      number = 0;
+      $(".bottom-content > span").text(number);
+    }
+  });
+});
---
+++ javascript/260320_homework/vanilla.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  var createButton = document.querySelector(".create-content");
+  var deleteAll = document.querySelector(".delete-all");
+  var ul = document.querySelector("ul");
+  var number = 0;
+  var span = document.querySelector("span");
+
+  createButton.addEventListener("click", function () {
+    var listItem = document.createElement("li");
+    listItem.innerText = "아이템 목록" + ++number;
+
+    if (number < 10) {
+      ul.appendChild(listItem);
+      span.innerText = number;
+    } else {
+      alert("더 이상 추가할 수 없습니다.");
+    }
+  });
+
+  deleteAll.addEventListener("click", function () {
+    if (number === 0) {
+      alert("이미 모든 아이템이 제거되었습니다.");
+    } else {
+      //   var listItems = ul.querySelectorAll("li");
+      //   for (var i = 0; i < listItems.length; i++) {
+      //     listItems[i].remove();
+      //   }
+      // listItems.forEach(function (li) {
+      //   li.remove();
+      // });
+
+      // ul.replaceChildren();
+      ul.innerHTML = "";
+    }
+    number = 0;
+    span.innerText = number;
+  });
+};
